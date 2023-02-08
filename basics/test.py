48a3c1a593916901fa5bc883a8c9cab37e00435f
Robertson-98 <warface_213243@gmail.ru>
2023-02-08 13:43:37 +0600

    lists

--- basics/test.py
+++ basics/test.py
@@ -1,57 +1,95 @@
-# inp1 = int(input('vvedite: '))
-# print(inp1)
+# # inp1 = int(input('vvedite: '))
+# # print(inp1)
 
-# import math
-# radius = 7 
-# s = pow(radius,2) * math.pi
-# print(s)
-
-
-# num = 7
-# p = num *4
-# s = num * num
-# print(p, s)
-
-num = int(input ("Введите число:"))
-if num > 0:
-    print(f"число {num} - положительное")
-elis num  == 0:
-    print(f"число {num} - этто 0")
-else
+# # import math
+# # radius = 7 
+# # s = pow(radius,2) * math.pi
+# # print(s)
 
 
-#============================================================================================
+# # num = 7
+# # p = num *4
+# # s = num * num
+# # print(p, s)
 
-password = input ("придумайте свой пароль: ")
-first_let = password[0].upper()
-print(first_let)
-
-if len(password) <= 8:
-    print("Ваш пароль меньше 8 символов")                 #- код для создания пароля.
-
-elis not password.startswith(upper_let):
-    print("Ваш пароль не начинается с большой буквы") 
-
-else:
-    print( "Успешно создан пароль")
-============================================================================================
-                      ТЕРНАРНЫЕ ОПЕРАТОРЫ
-
- num = int(imput())
-
- res ="HELLO" if num == 5 else "bye"
- print(res)                     
-=======================================================================================
-
-num = ist(input())
-
-if num % 3 == 0 and num % 5 == 0:
-    print("Fizz")
-elif num % 5 == 0:
-    print("Buzz")
-elif num % 3 == 0:
-    print    
+# num = int(input ("Введите число:"))
+# if num > 0:
+#     print(f"число {num} - положительное")
+# elis num  == 0:
+#     print(f"число {num} - этто 0")
+# else
 
 
-pop = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print(reversed(pop))
+# #============================================================================================
+
+# password = input ("придумайте свой пароль: ")
+# first_let = password[0].upper()
+# print(first_let)
+
+# if len(password) <= 8:
+#     print("Ваш пароль меньше 8 символов")                 #- код для создания пароля.
+
+# elis not password.startswith(upper_let):
+#     print("Ваш пароль не начинается с большой буквы") 
+
+# else:
+#     print( "Успешно создан пароль")
+# ============================================================================================
+#                       ТЕРНАРНЫЕ ОПЕРАТОРЫ
+
+#  num = int(imput())
+
+#  res ="HELLO" if num == 5 else "bye"
+#  print(res)                     
+# =======================================================================================
+
+# num = ist(input())
+
+# if num % 3 == 0 and num % 5 == 0:
+#     print("Fizz")
+# elif num % 5 == 0:
+#     print("Buzz")
+# elif num % 3 == 0:
+#     print    
+#--------------------------------------------------------------------------------------------
+
+# pop = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(reversed(pop))
+
+# pop ="hello"
+# chr
+#----------------------------------------------------------------------------------------------
+
+# string = 'Hello!'
+# res = []
+# for i in string:
+#     res.append(ord(i))
+# print(res)
+# # print(ord('s'))
+
+#-----------------------------------------------------------------------------------------------
+# mark = int(input())
+# if mark >= 90:
+#     print("Отлично, Bаша оценка 5!")
+# elif mark >= 80:
+#     print("Здорово, Ваша оценка 4!")
+# elif mark >= 70:
+#     print("Хорошо, Ваша оценка 3!")
+# elif mark >= 60:
+#     print("Вам стоит подучить материал")
+# else:
+#     print("Вы не сдали экзамен")
+#-----------------------------------------------------------------------------------------------
+# number = 0.1
+# if number < 0:
+#     print("negative")
+# if number > 0:
+#     print("positive")    
+# elif number == 0:
+#     print("zero")
+#--------------------------------------------------------------------------------------------
+
+list1 = [ 1,2,3,1,2,4,5,9,1,1,5,7,7]
+while 1 in list1:
+    list1.remove(1)
+print(list1)
