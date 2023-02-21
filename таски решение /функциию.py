5d1d3723ae237f478e4c1a2e40c410865684995a
Robertson-98 <warface_213243@gmail.ru>
2023-02-21 13:00:46 +0600

    built-in-functions

---
+++ таски решение /функциию.py
@@ -0,0 +1,55 @@
+##========================2
+
+# def get_string_length(pop):
+#     count = 0
+#     for i in pop:
+#         count += 1
+#     return count
+# print(get_string_length("dfdghejfgh"))
+
+##=======================3
+
+# def get_type(a, b):
+#     print(f"{type(a)}\n{type(b)}")
+
+##=======================4
+
+# def divide(a, b):
+#     return a / b
+
+##=======================5
+
+# dict_ = dict(a=4, b=8, c=9)
+
+# def dictionary(dict_):
+#     for i in dict_:
+#         print(i)
+
+##======================6
+
+# num = 6
+# def check(num): 
+#     if num % 2 == 0: 
+#         return("It is even number") 
+#     else: 
+#         return("It is odd number")
+
+##======================7
+
+# def is_palindrome(str): 
+#     if str.upper() == str.upper()[::-1]: 
+#         return True 
+#     else: 
+#         return False 
+# print(is_palindrome("Mom"))
+
+##======================8
+
+# def max_num(a, b):
+#     return max(a,b)
+
+# max_num(10, 34)
+
+##======================9
+
+
